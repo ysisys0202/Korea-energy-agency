2a7b0e48fb071e9a79f6431363b036cae6a98269
yahoneogul <yahoneogul.gmail.com>
2021-02-28 21:12:16 +0900

    fullpage content, pagination active 추가

--- js/fullpage-animation.js
+++ js/fullpage-animation.js
@@ -1,34 +1,81 @@
 const container = document.querySelector(".container");
 const contents = document.querySelectorAll(".content");
+const pagination = document.querySelector(".content-pagination");
+const paginationItems = document.querySelectorAll(".content-pagination-item");
+const footer = document.querySelector(".global-footer");
+const footerHeight = footer.getBoundingClientRect().height;
 let flag = true;
 let contentIdx = 0;
+let currentContent = null;
+let currentPaginationItem = null;
 
+//데이터인덱스 세팅
 const dataIdxSetting = (elements) => {
   elements.forEach((element, index) => {
     element.dataset.index = index;
   });
 };
+
+//콘텐트 바꾸기
 const changeContent = (contentIdx) => {
   container.style.transform = `translateY(${-100 * contentIdx}vh)`;
   flag = false;
 };
+//다음 콘텐트로 바꾸기
+const goToNextContent = () => {
+  ++contentIdx;
+  changeContent(contentIdx);
+};
+//이전 콘텐트로 바꾸기
+const goToPrevContent = () => {
+  --contentIdx;
+  changeContent(contentIdx);
+};
+//요소 비활성화
+const inactiveElement = (elements) => {
+  elements.forEach((element) => {
+    element.classList.remove("active");
+  });
+};
+//요소 활성화
+const activateElement = (contentIdx, elements) => {
+  elements[contentIdx].classList.add("active");
+};
 
-dataIdxSetting(contents);
+const paginationHandler = (e) => {
+  const targetItem = e.target;
+  const targetIdx = targetItem.dataset.index;
+  inactiveElement(paginationItems);
+  if (targetItem.classList.contains("content-pagination-item")) {
+    activateElement(targetIdx, paginationItems);
+  }
+  contentIdx = targetIdx;
+  changeContent(targetIdx);
+};
 
 const wheelHandler = (e) => {
   wheelDelta = e.deltaY;
+  //휠하면 콘텐츠 바뀜
   if (flag === true) {
     if (wheelDelta > 0 && contentIdx < contents.length - 1) {
-      ++contentIdx;
-      changeContent(contentIdx);
+      goToNextContent();
     } else if (wheelDelta < 0 && contentIdx > 0) {
-      --contentIdx;
-      changeContent(contentIdx);
+      goToPrevContent();
     }
   }
   container.addEventListener("transitionend", () => {
     flag = true;
   });
-  console.log(contentIdx);
+
+  inactiveElement(contents);
+  activateElement(contentIdx, contents);
+  inactiveElement(paginationItems);
+  activateElement(contentIdx, paginationItems);
 };
+
+dataIdxSetting(contents);
+dataIdxSetting(paginationItems);
+activateElement(0, contents);
+activateElement(0, paginationItems);
 window.addEventListener("wheel", wheelHandler);
+pagination.addEventListener("click", paginationHandler);
